f34fbc150a1a0922edac6f592522798846ebecb5
kaunghtethtun <kaunghtethtun2013@gmail.com>
2026-06-04 01:13:43 +0630

    assignment-6_khh

---
+++ assignment-submission/class-13and14/kaung_htet_htun_assignment-6/class-13and14/data/g2p-par/bleu_improve_workspace/experiments/norm_5g_p5_rr30/mert/extractor.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /home/phantom/Desktop/Git/AI_Assignment/assignment-6/class-13and14/data/g2p-par/bleu_improve_workspace/experiments/norm_5g_p5_rr30/mert
+/home/phantom/mosesdecoder/bin/extractor --sctype BLEU --scconfig case:true  --scfile run6.scores.dat --ffile run6.features.dat -r /home/phantom/Desktop/Git/AI_Assignment/assignment-6/class-13and14/data/g2p-par/bleu_improve_workspace/normalized/dev.ph -n run6.best100.out.gz
